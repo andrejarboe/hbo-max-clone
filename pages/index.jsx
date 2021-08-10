086dbe2d9fc06836f33b1429b351fb4167c837a1
andrejarboe <andrejarboe@gmail.com>
2021-08-10 15:06:19 -0400

    add login componet and scss for it

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,27 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1>
-      Test
-    </h1>
+    <div>
+      <div className="login-user">
+        <div className="login-user__top">
+          <div className="login-user__logo" />
+          <span className="login-user__title">
+            Who is Watching?
+          </span>
+        </div>
+        <div className="login-user__form">
+          <div className="login-user__user-box">
+            <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=046c29138c1335ef8edee7daf521ba50" alt="" className="login-user__user-img" />
+            <div className="login-user__user-name">Stephanie</div>
+          </div>
+        </div>
+        <div className="login-user__buttons">
+          <button className="login-user__adult">Add Adult</button>
+          <button className="login-user__kid">Add Kid</button>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
