ef9459fbb2e9a9c82b020a9d3da46660e1d139ac
andrejarboe <andrejarboe@gmail.com>
2021-08-10 16:45:04 -0400

    make header ui component

--- components/Layouts/MainLayout.jsx
+++ components/Layouts/MainLayout.jsx
@@ -2,12 +2,14 @@ import Header from '../UI/Header'
 
 const MainLayout = (props) => {
     return (
-        <>
+        <div style={{
+            background: 'linear-gradient(135deg, rgba(0,0,0,1) 55%, rgba(119,30,135,1) 100%)', minHeight: '100vh', backgroundAttachment: 'fixed'
+          }}>
             <Header />
             <section className="content-container">
                 {props.children}
             </section>
-        </>
+        </div>
     )
 }
 export default MainLayout;
--- components/UI/Header.jsx
+++ components/UI/Header.jsx
@@ -1,7 +1,20 @@
 const Header = (props) => {
-    return(
-        <header>
-header
+    return (
+        <header className="top-header">
+            <div className="top-header__left-side">
+                <div className="top-header__menu-btn">
+                    <i className="fas fa-bars" />
+                </div>
+                <div className="top-header__search-btn">
+                    <i className="fas fa-search" />
+                </div>
+            </div>
+            <div className="top-header__logo"></div>
+            <div className="top-header__account">
+                <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=046c29138c1335ef8edee7daf521ba50" className="top-header__user-img" alt="" />
+                <div className="top-header__user-name">Stephanie</div>
+            </div>
+
         </header>
     )
 }
