d2f03a2dd5742d33312d445fe00d41a9526cff18
andrejarboe <andrejarboe@gmail.com>
2021-08-11 09:50:45 -0400

    finish account component

--- components/UI/Header/Header.jsx
+++ components/UI/Header/Header.jsx
@@ -1,3 +1,5 @@
+import Account from "../Account/Account";
+
 const Header = (props) => {
     return (
         <header className="top-header">
@@ -14,7 +16,7 @@ const Header = (props) => {
                 <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=046c29138c1335ef8edee7daf521ba50" className="top-header__user-img" alt="" />
                 <div className="top-header__user-name">Stephanie</div>
             </div>
-
+            <Account />
         </header>
     )
 }
