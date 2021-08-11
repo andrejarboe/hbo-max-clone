fa9b2a97771949ecf04f82b6a52185ce2bda73ae
andrejarboe <andrejarboe@gmail.com>
2021-08-10 21:43:59 -0400

    finish for you list temp

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,11 +1,12 @@
 import MainLayout from "../components/Layouts/MainLayout";
 import FeaturedMedia from "../components/UI/Featuredmedia/FeaturedMedia";
+import ForYouList from "../components/UI/ForYouList/ForYouList";
 
 export default function HomeView() {
     return (
         <MainLayout>
             <FeaturedMedia />
-      
+            <ForYouList />
         </MainLayout>
     )
 }
