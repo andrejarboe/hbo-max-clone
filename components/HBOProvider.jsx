690431cdf649cd83baa5e99e5d7138ed1adc9575
andrejarboe <andrejarboe@gmail.com>
2021-08-12 20:37:13 -0400

    open side nav and account menu

--- components/HBOProvider.jsx
+++ components/HBOProvider.jsx
@@ -10,11 +10,15 @@ export function useStateContext() {
 export function HBOProvider({ children }) {
     const [user, setUser] = useState('')
     const defaultUserImg = 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=046c29138c1335ef8edee7daf521ba50'
-    
+
     const createUserAction = (e) => {
         setUser(e.target.value)
         console.log(user);
     }
+
+    const [sideNavOpen, setSideNavOpenAction] = useState(false);
+    const [accountModalOpen, setAccountModalOpenAction] = useState(false);
+
 
     return (
 
@@ -22,7 +26,11 @@ export function HBOProvider({ children }) {
             value={{
                 user,
                 createUserAction,
-                defaultUserImg
+                defaultUserImg,
+                sideNavOpen,
+                setSideNavOpenAction,
+                accountModalOpen, 
+                setAccountModalOpenAction
             }}
         >
             {children}
