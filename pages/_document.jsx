065b679dd4cec1109171f7e4e27fcf231b78abc9
andrejarboe <andrejarboe@gmail.com>
2021-08-13 14:23:35 -0400

    add fondawesome to public folder

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,7 +13,7 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                     <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700&display=swap" />
-                    <link rel="preload" as="style" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+                    <link rel="stylesheet" href="/fontawesome/css/all.min.css" />
 
                 </Head>
                 <body>
